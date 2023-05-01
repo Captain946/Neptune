6f6eab8b8cf09a9197aadb752a2ba4db364b0e92
loharmanish10 <116274465+loharmanish10@users.noreply.github.com>
2023-05-01 15:44:02 +0530

    remove unessesary data

--- src/main/java/ManishLokesh/Neptune/Users/RespondeBody/SignUpResponse.java
+++ src/main/java/ManishLokesh/Neptune/Users/RespondeBody/SignUpResponse.java
@@ -1,31 +1,15 @@
 package ManishLokesh.Neptune.Users.RespondeBody;
 
-import ManishLokesh.Neptune.Users.Entity.Signup;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
 public class SignUpResponse {
     private String status;
     private String message;
     private String result;
-    private Long id;
-    private String mobileNumber;
-    private String emailId;
-    private String gender;
-    private String fullName;
-    private String createdAt;
-    private String updatedAt;
-    private String  otp;
-
-    public SignUpResponse(){
-        super();
-    }
 
     public SignUpResponse(String status,String message, String result){
         this.status = status;
         this.message = message;
         this.result = result;
     }
-
 
     public String getStatus() {
         return status;
@@ -49,70 +33,5 @@ public class SignUpResponse {
 
     public void setResult(String result) {
         this.result = result;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(String updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-
-    public String getOtp() {
-        return otp;
-    }
-
-    public void setOtp(String otp) {
-        this.otp = otp;
     }
 }
