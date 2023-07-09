646033f5008e5a505bdf45fbfec9e4fcb125bda0
loharmanish10 <116274465+loharmanish10@users.noreply.github.com>
2023-07-09 11:09:13 +0530

    bug fix

--- src/main/java/ManishLokesh/Neptune/v2/Orders/RequestBody/OrderRequestBody.java
+++ src/main/java/ManishLokesh/Neptune/v2/Orders/RequestBody/OrderRequestBody.java
@@ -6,13 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-@Service
-public class OrderRequestBody implements OrderService {
 
-    @Override
-    public ResponseEntity<ResponseDTO> addOrder(OrderRequestBody orderRequestBody) {
-        return new ResponseEntity<>(
-                new ResponseDTO("success", null, "Order Created successfully"),
-                HttpStatus.CREATED);
-    }
+public class OrderRequestBody {
+
+
 }
---
+++ src/main/java/ManishLokesh/Neptune/v2/Orders/Service/OrderServiceImp.java
@@ -0,0 +1,18 @@
+package ManishLokesh.Neptune.v2.Orders.Service;
+
+import ManishLokesh.Neptune.ResponseDTO.ResponseDTO;
+import ManishLokesh.Neptune.v2.Orders.RequestBody.OrderRequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class OrderServiceImp implements OrderService{
+    @Override
+    public ResponseEntity<ResponseDTO> addOrder(OrderRequestBody orderRequestBody) {
+        return new ResponseEntity<>(
+                new ResponseDTO("success", null, "Order Created successfully"),
+                HttpStatus.CREATED);
+    }
+}
