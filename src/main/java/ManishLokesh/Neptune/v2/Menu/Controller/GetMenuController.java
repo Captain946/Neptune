4abfd62577fa6898ced0b005c7a0921f44baa44a
manish <manish.l@ipsator.com>
2023-08-12 20:39:29 +0530

    add security in get outlets and get menu items

--- src/main/java/ManishLokesh/Neptune/v2/Menu/Controller/GetMenuController.java
+++ src/main/java/ManishLokesh/Neptune/v2/Menu/Controller/GetMenuController.java
@@ -1,22 +1,33 @@
 package ManishLokesh.Neptune.v2.Menu.Controller;
 
+import ManishLokesh.Neptune.AuthController.JwtUtil;
 import ManishLokesh.Neptune.ResponseDTO.ResponseDTO;
 import ManishLokesh.Neptune.v2.Menu.Service.GetMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class GetMenuController {
 
     @Autowired
     public GetMenuService service;
+    @Autowired
+    public JwtUtil jwtUtil;
 
     @GetMapping("api/v2/outlet/{outlet_Id}/menu")
-    public ResponseEntity<ResponseDTO> getMenu(@PathVariable("outlet_Id") String outletId){
-        return this.service.getActiveMenu(outletId);
+    public ResponseEntity<ResponseDTO> getMenu(@PathVariable("outlet_Id") String outletId, @RequestHeader(HttpHeaders.AUTHORIZATION) String auth) {
+        String token = auth.replace("Bearer","");
+        if(jwtUtil.validateToken(token)){
+            return this.service.getActiveMenu(outletId);
+        }
+        return new ResponseEntity<>(new ResponseDTO("failure","Not authorize to Access",null),HttpStatus.UNAUTHORIZED);
+
     }
 }
--- src/main/java/ManishLokesh/Neptune/v2/Outlets/Controller/GetOutletController.java
+++ src/main/java/ManishLokesh/Neptune/v2/Outlets/Controller/GetOutletController.java
@@ -1,21 +1,32 @@
 package ManishLokesh.Neptune.v2.Outlets.Controller;
 
+import ManishLokesh.Neptune.AuthController.JwtUtil;
 import ManishLokesh.Neptune.ResponseDTO.ResponseDTO;
 import ManishLokesh.Neptune.v2.Outlets.Service.GetOutletService;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class GetOutletController {
 
     @Autowired
     public GetOutletService getOutletService;
+    @Autowired
+    public JwtUtil jwtUtil;
 
     @GetMapping("/api/v2/outlet/station/{stationCode}")
-    public ResponseEntity<ResponseDTO> GetOutlet(@PathVariable String stationCode){
-        return this.getOutletService.GetOutletAll(stationCode);
+    public ResponseEntity<ResponseDTO> GetOutlet(@PathVariable String stationCode, @RequestHeader(HttpHeaders.AUTHORIZATION) String auth) {
+        String token = auth.replace("Bearer","");
+        if (jwtUtil.validateToken(token)){
+            return this.getOutletService.GetOutletAll(stationCode);
+        }
+        return new ResponseEntity<>(new ResponseDTO("failure","Not authorize to Access",null), HttpStatus.UNAUTHORIZED);
     }
 }
